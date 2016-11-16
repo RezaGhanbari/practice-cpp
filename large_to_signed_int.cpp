bc562eda2f83d7d7b3280eb986829b81e8439d64
Reza <rezapython@gmail.com>
2016-11-16 12:28:58 +0330

    assign too large number to a signed integer

---
+++ large_to_signed_int.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    short int smallNumber;
+    smallNumber = 32767;
+    cout << "small number: " << smallNumber << endl;
+    smallNumber++;
+    cout << "small number: " << smallNumber << endl;
+    smallNumber++;
+    cout << "small number: " << smallNumber << endl;
+}
