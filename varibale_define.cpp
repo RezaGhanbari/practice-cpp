ed9fa5dfdf863dc1a865ac6bb1614d424a5edaa7
Reza <rezapython@gmail.com>
2016-11-16 12:14:27 +0330

    variable define

---
+++ varibale_define.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//    unsigned short x;
+//    unsigned short y;
+//    unsigned long z;
+//    z = x*y;
+//    unsigned short width;
+//    unsigned short length;
+//    unsigned short area;
+//    area = width*length;
+//
+//    unsigned int myAge, myWeight; // two unsigned int variables
+//    long a, w, l;                // three longs
+    unsigned short int width = 5, length;
+    length = 10;
+    // create an unsigned short and initialize with result
+
+    // of multiplying Width by Length
+
+    unsigned short int area = width * length;
+    cout << "Width:" << width << "\n";
+    cout << "Length: " << length << endl;
+    cout << "Area: " << area << endl;
+
+    return 0;
+
+}
+
