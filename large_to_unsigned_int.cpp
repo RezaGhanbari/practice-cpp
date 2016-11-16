a8226813bd719545360c03276af388bafbf51052
Reza <rezapython@gmail.com>
2016-11-16 12:24:01 +0330

    assign too large number to an unsigned integer

---
+++ large_to_unsigned_int.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    unsigned short int smallNumber;
+    smallNumber = 65535;
+    cout << "small number is: " << smallNumber << endl;
+    smallNumber++;
+    cout << "small number is: " << smallNumber << endl;
+    smallNumber++;
+    cout << "small number is: " << smallNumber << endl;
+
+    return 0;
+}
