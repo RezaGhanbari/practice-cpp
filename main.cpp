9d30df5e8231338aef6d88abaf81c61a6614e5c3
Reza <rezapython@gmail.com>
2016-11-16 11:08:36 +0330

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// listing 2.2 using cout
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello there. \n";
+    cout << "Here is 5: " << 5 << "\n";
+    cout << "The manipulator endl writes a new line to the screen." << endl;
+    cout << "Here is a very big number: \t" << 70000 << endl;
+    cout << "Here is the sum of 8 and 5: \t" << 8+5 << endl;
+    cout << "Here's a fraction : \t\t" << (float) 5/8 << endl;
+    cout << "And a very very big number: \t\t" << (double) 70000 * 70000 << endl;
+    cout << "Reza Ghanbari is a C++ programmer!\n";
+
+    return 0;
+
+}
